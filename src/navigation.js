9256c5326efa0a952b4a9fd87fb73452675e0e0e
devinschumacher <devin@serp.co>
2024-01-08 02:12:15 -0800

    lots of progress

--- src/navigation.js
+++ src/navigation.js
@@ -3,178 +3,126 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Boxing',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+          text: 'Boxers',
+          href: getPermalink('/boxers'),
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+          text: 'Divisions',
+          href: getPermalink('/weight-classes'),
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+          text: 'Fights',
+          href: getPermalink('/fights'),
         },
         {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+          text: 'Schedule',
+          href: getPermalink('/schedule'),
+        },
+        {
+          text: 'Tickets',
+          href: getPermalink('/tickets'),
         },
       ],
     },
     {
-      text: 'Pages',
+      text: 'Videos',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'YouTube',
+          href: getPermalink('/boxers'),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
+          text: 'Full Fights',
+          href: getPermalink('/weight-classes'),
         },
       ],
     },
     {
-      text: 'Landing',
+      text: 'Content',
       links: [
         {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
+          text: 'Blog',
           href: getBlogPermalink(),
         },
         {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+          text: 'Categories',
+          href: getPermalink('blog', 'category'),
         },
         {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
+          text: 'Glossary',
+          href: getPermalink('/glossary'),
         },
       ],
     },
     {
-      text: 'Widgets',
-      href: '#',
+      text: 'Shop',
+      links: [
+        {
+          text: 'Shop',
+          href: getPermalink('/shop'),
+        },
+        {
+          text: 'Categories',
+          href: getPermalink('/shop/best'),
+        },
+
+      ],
     },
+   
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [{ text: 'Subscribe', href: '/subscribe', target: '_blank' }],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
       title: 'Company',
       links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Website', href: 'https://boxingundefeated.com' },
+        { text: 'About', href: '/about' },
+      ],
+    },
+    {
+      title: 'Community',
+      links: [
+        { text: 'Newsletter', href: '/subscribe' },
+        { text: 'Forum', href: '/#' },
+        { text: 'Community', href: '/#' },
+      ],
+    },
+    {
+      title: 'Links',
+      links: [
+        { text: 'Substack', href: 'https://boxingundefeated.substack.com/' },
+        { text: 'Hashnode', href: 'https://boxingundefeated.hashnode.dev/' },
+      ],
+    },
+    {
+      title: 'Boring Stuff',
+      links: [
+        { text: 'Privacy Policy', href: '/privacy' },
+        { text: 'Terms & Conditions', href: '/terms' },
+        { text: 'Affiliate Disclosure', href: '/affiliate-disclosure' },
+        { text: 'DMCA', href: '/dmca' },
+        { text: 'Archive', href: '/archive' },
+        { text: 'Tags', href: '/tags' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Free Stuff', href: 'https://stuff.boxingundefeated.com' },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://serp.ly/@boxingundefeated/twitter' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://serp.ly/@boxingundefeated/instagram' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/boxundefeated' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://serp.ly/@boxingundefeated/youtube' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-linkedin', href: 'https://linkedin.com/company/boxingundefeated' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-tiktok', href: 'https://tiktok.com/@boxundefeated' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-medium', href: 'https://medium.com/boxing-undefeated' },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
